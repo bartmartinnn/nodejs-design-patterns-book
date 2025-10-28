fa78213459bac623e8cb6e55bcee693ef208e553
bartmartinnn <bartmartinnn@gmail.com>
2025-10-28 11:21:44 +0100

    finished ticker

--- 03-callbacks-and-events/exercises/ticker.js
+++ 03-callbacks-and-events/exercises/ticker.js
@@ -4,22 +4,48 @@ function ticker(number, cb) {
     const eventEmitter = new EventEmitter()
     let counter = 0
 
+    function checkIfDateDivisibleBy5(){
+        if (Date.now() % 5 === 0) {
+            eventEmitter.emit('error')
+            cb('divisible by 5', number)
+            return true
+        } else {
+            return false
+        }
+    }
+
     function tick() {
         setTimeout(() => {
             eventEmitter.emit('tick')
             counter++
             number -= 50
             if (number < 50){
-                cb(counter)
+                cb(null, counter)
+            } else if (checkIfDateDivisibleBy5()) {
+
             } else {
                 tick()
             }
         }, 50)
     }
 
+    process.nextTick(() => {
+        checkIfDateDivisibleBy5()
+        eventEmitter.emit('tick')
+        counter++
+    })
+
     tick()
     return eventEmitter
 }
 
-ticker(200,(number) => console.log(`Finished with "${number}"`))
+ticker(200,(error, number) => {
+    if (error) {
+        console.log(`callback error "${error}"`)
+    } else {
+        console.log(`Finished with "${number}"`)
+    }
+
+})
 .on('tick', () => console.log('tick happened'))
+.on('error', () => console.log('error happened!'))
