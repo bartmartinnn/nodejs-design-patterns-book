6b60ae1313a133dcec6571ea45d56546e685f855
bartmartinnn <bartmartinnn@gmail.com>
2025-10-30 11:54:30 +0100

    finished excercise 1

--- 04-asynchronous-control-flow-patterns-with-callbacks/excercises/01-file-concatenation.js
+++ 04-asynchronous-control-flow-patterns-with-callbacks/excercises/01-file-concatenation.js
@@ -1,18 +1,26 @@
 import { readFile, writeFile } from 'node:fs'
 import { join } from 'node:path'
 
-function concatFiles(srcFile1, srcFile2, dest, cb){
-    // copy the contents of every source file into the destination file
-    // respecting the order of the files, as in the arg list
-    readFile(srcFile1, (err, data1) => {
-        readFile(srcFile2, (err, data2) => {
-            console.log(data1.toString(), data2.toString())
-            let content = data1.toString() + data2.toString()
+function concatFiles(dest, cb, ...files){
+    //there can be multiple files
+    let content = ''
+    function iterate (index) {
+        if (index === files.length){
             writeFile(dest, content, cb)
+            return cb()
+        }
+        const file = files[index]
+        readFile(file, (err, data) => { 
+            if(err){
+                return cb()
+            }
+            content += data.toString()
+            iterate(index+1)
         })
-    })
+    }
+    iterate(0)
 }
 
-concatFiles('./foofile.txt', './barfile.txt', './destfile.txt', () => {
+concatFiles('./destfile.txt', () => {
     console.log('finished')
-})
+}, './foofile.txt', './barfile.txt', './bgfile.txt')
