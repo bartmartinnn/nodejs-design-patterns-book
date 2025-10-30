36914669a2daddea11210beff0c04cec07871115
bartmartinnn <bartmartinnn@gmail.com>
2025-10-30 14:39:42 +0100

    cleaned up a bit

--- 04-asynchronous-control-flow-patterns-with-callbacks/excercises/02-list-files-recursively.js
+++ 04-asynchronous-control-flow-patterns-with-callbacks/excercises/02-list-files-recursively.js
@@ -4,71 +4,58 @@ import {join} from 'node:path'
 function listNestedFiles(path, cb){
     let listOfFiles = []
     function iterate(path, cb) {
-        stat(path, (err, stats) => {
+        readdir(path, (err, files) => {
             if (err) {
-                console.error('Error reading path', err.message)
+                console.error('Error reading path')
                 return cb(err)
             }
 
-            if (!stats.isDirectory()){
-                console.log('not a directory!')
+            let pending = files.length
+
+            if (pending === 0) {
+                console.log('directory is empty')
                 return cb(null, [])
             }
 
-            readdir(path, (err, files) => {
-                if (err) {
-                    console.error('Error reading path')
-                    return cb(err)
-                }
-
-                let pending = files.length
-
-                if (pending === 0) {
-                    console.log('directory is empty')
-                    return cb(null, [])
-                }
-
-                let results = []
-
-                for(let i=0; i<files.length; i++) {
-                    const filePath = join(path, files[i])
-                    stat(filePath, (err, stats) => {
-                        if (err){
-                            console.log('error in inner stat: ', err)
+            for(let i=0; i<files.length; i++) {
+                const filePath = join(path, files[i])
+                stat(filePath, (err, stats) => {
+                    if (err){
+                        console.log('error in inner stat: ', err)
+                        pending--
+                        if (pending === 0) {
+                            cb(null, listOfFiles)
+                        }
+                        return
+                    }
+                    if (stats.isDirectory()){
+                        console.log('have a directory here:', files[i])
+                        iterate(filePath, (err, subFiles) => {
+                            if (!err && subFiles) {
+                                listOfFiles.push(...subFiles)
+                            }
                             pending--
-                            if (pending === 0) {
+                            if(pending ===0){
                                 cb(null, listOfFiles)
                             }
-                            return
+                        })
+                    } else {
+                        listOfFiles.push(filePath)
+                        pending--
+                        if(pending === 0) {
+                            cb(null, listOfFiles)
                         }
-                        if (stats.isDirectory()){
-                            console.log('have a directory here:', files[i])
-                            iterate(filePath, (err, subFiles) => {
-                                if (!err && subFiles) {
-                                    listOfFiles.push(...subFiles)
-                                }
-                                pending--
-                                if(pending ===0){
-                                    cb(null, listOfFiles)
-                                }
-                            })
-                        } else {
-                            listOfFiles.push(filePath)
-                            pending--
-                            if(pending === 0) {
-                                cb(null, listOfFiles)
-                            }
-                        }
-                    })
-                }
-            })
+                    }
+                })
+            }
         })
-    }
+}
+
     iterate(path, (err, files) => {
         if(err){
             return cb(err)
         }
-        cb(null, listOfFiles)
+        cb(null, files)
 })
 }
 
